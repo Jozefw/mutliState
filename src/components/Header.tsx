e76ba2e1f8298ff2db2a45ab66693249a77fb516
Jozef <Jozefw@sbcglobal.net>
2025-03-12 09:02:13 -0700

    first part of context

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
+import {useContext} from 'react';
 import Button from './UI/Button.tsx';
+import TimersContext from './store/timers-context'
+
 
 export default function Header() {
+const timerContext = useContext(TimersContext);
+
   return (
     <header>
       <h1>ReactTimer</h1>
-
       <Button>Stop Timers</Button>
     </header>
   );
--- src/components/store/timers-context.tsx
+++ src/components/store/timers-context.tsx
@@ -1,7 +1,35 @@
-import React, {createContext} from 'react'
+import React, {type ReactNode,createContext} from 'react'
 
-export default function timers - context() {
-    return (
-        <div>timers-context</div>
-    )
+type SingleTimer = {
+    name:string;
+    duration: number;
+}
+
+type TimersState={
+    isRunning: boolean;
+    timers:SingleTimer[]
+}
+
+type TimersContextValue = TimersState & {
+    addTimer:(timerData:SingleTimer)=>void,
+    startTimers:()=>void,
+    stopTimers:()=>void,
+}
+
+type TimersContextProviderProps ={
+    children:ReactNode,
+}
+// context takes a value but null is good for now 
+export const TimersContext = createContext<TimersContextValue | null>(null)
+
+const contextObject: TimersContextValue ={
+    addTimer:(timerData)=>{},
+    startTimers:()=>{},
+    stopTimers:()=>{},
+    isRunning: false,
+    timers:[]
+}
+// TCP manages the state 
+export default function TimersContextProvider({children}:TimersContextProviderProps){
+    return <TimersContext.Provider value={contextObject}>{children}</TimersContext.Provider>
 }
